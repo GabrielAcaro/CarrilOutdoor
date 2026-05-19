dc50070dcc5e22d0e4223cb254c75339c6ed97d8
1DAM - Gabriel Acaro Sanchez <gacaros01@informatica.iesvalledeljerteplasencia.es>
2026-05-19 14:03:43 +0200

    clase informeResultado actualizada y completada

--- src/main/java/com/carriloutdoor/comparadorcsvproductos/Main.java
+++ src/main/java/com/carriloutdoor/comparadorcsvproductos/Main.java
@@ -12,18 +12,44 @@ import main.java.com.carriloutdoor.comparadorcsvproductos.service.InformeResulta
  * @author Gabriel Acaro Sánchez
  */
 public class Main {
+    
+    //Variables constantes del programa
+    
+    public static final String[] COLUMNASERP = {
+        "1. Nombre del producto",
+        "2. Codigo",
+        "3. Codigo de barras / Ean",
+        "4. Codigo de referencia",
+        "5. Medida / Talla / Calibre",
+        "6. Nombre Proveedor",
+        "7. Observaciones"
+    };
+
+    public static final String[] COLUMNASPRESTASHOP = {
+        "1. Nombre del producto",
+        "2. ID",
+        "3. ID Combinacion",
+        "4. Codigo de barras / Ean",
+        "5. Codigo de referencia",
+        "6. Medida / Talla / Calibre",
+        "7. Atributos"
+    };
+    
+    
     // Variables static del programa (rutas de archivos)
-        private static String rutaCsvArtErp = "";
-        private static String rutaCsvSubArtErp = "";
-        private static String rutaCsvArtPrestaShop = "";
+        private static String rutaCsvArtErp = "C:\\Users\\gabri\\WorkSpace_Netbeans\\ProgramasArmeria\\ComparadorCSVProductos\\data\\entrada\\listadoArticulosERP.csv";
+        private static String rutaCsvSubArtErp = "C:\\Users\\gabri\\WorkSpace_Netbeans\\ProgramasArmeria\\ComparadorCSVProductos\\data\\entrada\\listadoSubarticulosERP.csv";
+        private static String rutaCsvArtPrestaShop = "C:\\Users\\gabri\\WorkSpace_Netbeans\\ProgramasArmeria\\ComparadorCSVProductos\\data\\entrada\\listadoPrestashop.csv";
         
     //Comienzo del metodo que muestra el menu del programa
     public static void menu() {
         System.out.println("\nIntroduzca una de estas opciones para usar el programa:\n");
             System.out.println("Mostrar la informacion completa del CSV seleccionado: 1)");
-            System.out.println("Mostrar la informacion del intervalo entre dos valores de fila introducidos del CSV seleccionado: 2)");
-            System.out.println("Mostrar la informacion del intervalo entre dos valores de columna introducidos del CSV seleccionado: 3)");
-            System.out.println("Buscar producto por nombre, ID combinacion, Codigo de Barras, etc: 4)");
+            System.out.println("Mostrar toda la informacion del intervalo entre dos valores de fila del CSV seleccionado: 2)");
+            System.out.println("Mostrar toda la informacion del intervalo entre dos valores de columna del CSV seleccionado: 3)");
+            System.out.println("Mostrar la informacion del intervalo entre dos valores de fila y dos de columna del CSV seleccionado: 4)");
+            System.out.println("Buscar producto por nombre, ID combinacion, Codigo de Barras, etc: 5)");
+            System.out.println("Salir del programa: 0)");
     }
     
     // Metodo de scanner global
@@ -57,7 +83,7 @@ public class Main {
         System.out.println("Bienvenido al comparador de CSV de CarrilOutdoor!\n");
         
         // Pedimos la ruta de los CSV y los cargamos a un ArrayList
-        pedirRutasCSV();
+        // pedirRutasCSV();
         CsvService.leerLineasCsv(rutaCsvArtErp, listaProductosERP, listaProductosPrestaShop);
         CsvService.leerLineasCsv(rutaCsvSubArtErp, listaProductosERP, listaProductosPrestaShop);
         CsvService.leerLineasCsv(rutaCsvArtPrestaShop, listaProductosERP, listaProductosPrestaShop);
@@ -69,7 +95,15 @@ public class Main {
             
             switch (userOption) {
                 case 1 -> {
-                    InformeResultadoService.mostrarInfoCompletaProductosERP(listaProductosERP, listaProductosPrestaShop);
+                    InformeResultadoService.pedirListaUserInfoCompleta(listaProductosERP, listaProductosPrestaShop);
+                }
+                
+                case 2 -> {
+                    InformeResultadoService.pedirListaUserInfoIntervaloFilas(listaProductosERP, listaProductosPrestaShop);
+                }
+                
+                case 3 -> {
+                    InformeResultadoService.pedirListaUserInfoIntervaloColumnas(listaProductosERP, listaProductosPrestaShop);
                 }
                 
                 case 0 -> {
