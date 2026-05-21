454502e8d58f97c9b885ddf9ab6a19bdb6ab7c8e
1DAM - Gabriel Acaro Sanchez <gacaros01@informatica.iesvalledeljerteplasencia.es>
2026-05-21 13:59:59 +0200

    Clase BuscadorProductosService updated

--- src/main/java/com/carriloutdoor/comparadorcsvproductos/service/BuscadorProductosService.java
+++ src/main/java/com/carriloutdoor/comparadorcsvproductos/service/BuscadorProductosService.java
@@ -12,180 +12,128 @@ import java.util.ArrayList;
  */
 public class BuscadorProductosService {
     public static void solicitarListaUser(ArrayList<ProductoERP> listaProductosERP, ArrayList<ProductoPrestaShop> listaProductosPrestaShop) {
-        int userOptionList = 0, userOptionField = 0, i = 0, id = 0, idCombinacion = 0;
-        boolean encontrado = false;
-        String nombre = "", codigo = "", ean = "", codReferencia = "", nombreProveedor = "";
+        int userOptionList = 0, userOptionField = 0;
 
-        System.out.println("Introduzca uno de estos numero para elegir la lista en la que buscar: ");
+        userOptionList = pedirUserOptionList();
+
+        switch (userOptionList) {
+            case 1 -> {
+                userOptionField = pedirUserOptionField(userOptionList);
+
+                busquedaERP(listaProductosERP, listaProductosPrestaShop, userOptionList, userOptionField);
+            }
+
+            case 2 -> {
+                userOptionField = pedirUserOptionField(userOptionList);
+
+                busquedaPrestashop(listaProductosERP, listaProductosPrestaShop,userOptionList, userOptionField);
+            }
+
+            default -> System.err.println("Introduzca un número entre el 1 y el 2!");
+        }
+    }
+
+    public static int pedirUserOptionList() {
+        int userOptionList = 0;
+
+        System.out.println("Introduzca uno de estos números para elegir la lista en la que buscar: ");
         System.out.println("Lista de productos ERP: 1)");
         System.out.println("Lista de productos Prestashop: 2)");
 
-        userOptionList = Integer.parseInt(Main.scanner());
+        try {
+            userOptionList = Integer.parseInt(Main.scanner());
+        } catch (NumberFormatException e) {
+            System.err.println("Solo se permiten numeros en este campo!");
+        }
+
+        return userOptionList;
+    }
+
+    public static int pedirUserOptionField(int userOptionList) {
+        int userOptionField = 0;
 
         if (userOptionList == 1) {
-            System.out.println("Introduzca uno de estos numeros para elegir por que campo buscar:\n");
+            System.out.println("Introduzca uno de estos números para elegir por que campo buscar:\n");
             mostrarOpcionesBusquedaERP();
-            userOptionField = Integer.parseInt(Main.scanner());
-
-            switch (userOptionField) {
-                case 1 -> {
-                    System.out.println("Introduzca el nombre a buscar en la lista: ");
-                    nombre = Main.scanner();
-
-                    while (i < listaProductosERP.size() && !encontrado) {
-                        if (nombre.equals(listaProductosERP.get(i).getNombre())) {
-                                        mostrarInfoProductosERPEncontrado(listaProductosERP, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 2 -> {
-                    System.out.println("Introduzca el codigo a buscar en la lista: ");
-                    codigo = Main.scanner();
-
-                    while (i < listaProductosERP.size() && !encontrado) {
-                        if (codigo.equals(listaProductosERP.get(i).getCodigo())) {
-                            mostrarInfoProductosERPEncontrado(listaProductosERP, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 3 -> {
-                    System.out.println("Introduzca el Codigo de Barras / Ean a buscar en la lista: ");
-                    ean = Main.scanner();
-
-                    while (i < listaProductosERP.size() && !encontrado) {
-                        if (ean.equals(listaProductosERP.get(i).getEan())) {
-                            mostrarInfoProductosERPEncontrado(listaProductosERP, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 4 -> {
-                    System.out.println("Introduzca el codReferencia a buscar en la lista: ");
-                    codReferencia = Main.scanner();
-
-                    while (i < listaProductosERP.size() && !encontrado) {
-                        if (codReferencia.equals(listaProductosERP.get(i).getCodReferencia())) {
-                            mostrarInfoProductosERPEncontrado(listaProductosERP, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 5 -> {
-                    System.out.println("Introduzca el nombreProveedor a buscar en la lista: ");
-                    nombreProveedor = Main.scanner();
-
-                    while (i < listaProductosERP.size() && !encontrado) {
-                        if (nombreProveedor.equals(listaProductosERP.get(i).getNombreProveedor())) {
-                            mostrarInfoProductosERPEncontrado(listaProductosERP, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
+            try {
+                userOptionField = Integer.parseInt(Main.scanner());
+            } catch (NumberFormatException e) {
+                System.err.println("Solo se permiten numeros en este campo!");
             }
         } else if (userOptionList == 2) {
-            System.out.println("Introduzca uno de estos numeros para elegir por que campo buscar:\n");
+            System.out.println("Introduzca uno de estos números para elegir por que campo buscar:\n");
             mostrarOpcionesBusquedaPrestaShop();
-            userOptionField = Integer.parseInt(Main.scanner());
-
-            switch (userOptionField) {
-                case 1 -> {
-                    System.out.println("Introduzca el nombre a buscar en la lista: ");
-                    nombre = Main.scanner();
-
-                    while (i < listaProductosPrestaShop.size() && !encontrado) {
-                        if (nombre.equals(listaProductosPrestaShop.get(i).getNombre())) {
-                            mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 2 -> {
-                    System.out.println("Introduzca el ID a buscar en la lista: ");
-                    id = Integer.parseInt(Main.scanner());
-
-                    while (i < listaProductosPrestaShop.size() && !encontrado) {
-                        if (id == listaProductosPrestaShop.get(i).getId()) {
-                            mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 3 -> {
-                    System.out.println("Introduzca el ID Combinacion a buscar en la lista: ");
-                    idCombinacion = Integer.parseInt(Main.scanner());
-
-                    while (i < listaProductosPrestaShop.size() && !encontrado) {
-                        if (idCombinacion == listaProductosPrestaShop.get(i).getIdCombinacion()) {
-                            mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 4 -> {
-                    System.out.println("Introduzca el Codigo de Barras / Ean a buscar en la lista: ");
-                    ean = Main.scanner();
-
-                    while (i < listaProductosPrestaShop.size() && !encontrado) {
-                        if (ean.equals(listaProductosPrestaShop.get(i).getEan())) {
-                            mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
-
-                case 5 -> {
-                    System.out.println("Introduzca el codReferencia a buscar en la lista: ");
-                    codReferencia = Main.scanner();
-
-                    while (i < listaProductosPrestaShop.size() && !encontrado) {
-                        if (codReferencia.equals(listaProductosPrestaShop.get(i).getCodReferencia())) {
-                            mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
-                            encontrado = true;
-                        } else {
-                            encontrado = false;
-                        }
-                        i++;
-                    }
-                }
+            try {
+                userOptionField = Integer.parseInt(Main.scanner());
+            } catch (NumberFormatException e) {
+                System.err.println("Solo se permiten numeros en este campo!");
             }
-        } else {
-            System.err.println("Introduzca un numero entre el 1 y el 2!");
+        }
+
+        return userOptionField;
+    }
+
+    public static void busquedaERP(ArrayList<ProductoERP> listaProductosERP, ArrayList<ProductoPrestaShop> listaProductosPrestaShop, int userOptionList, int userOptionField) {
+        switch (userOptionField) {
+            case 1 -> {
+                pedirNombre(listaProductosERP, listaProductosPrestaShop, userOptionList);
+            }
+
+            case 2 -> {
+                pedirCodigo(listaProductosERP);
+            }
+
+            case 3 -> {
+                pedirEan(listaProductosERP, listaProductosPrestaShop, userOptionList);
+            }
+
+            case 4 -> {
+                pedirCodReferencia(listaProductosERP, listaProductosPrestaShop, userOptionList);
+            }
+
+            case 5 -> {
+                pedirNombreProveedor(listaProductosERP);
+            }
+
+            case -1 -> {
+
+            }
+
+            default -> {
+                System.err.println("Elija un numero entre el 1 y el 5!");
+            }
+        }
+    }
+
+    public static void busquedaPrestashop(ArrayList<ProductoERP> listaProductosERP, ArrayList<ProductoPrestaShop> listaProductosPrestaShop, int userOptionList, int userOptionField) {
+        switch (userOptionField) {
+            case 1 -> {
+                pedirNombre(listaProductosERP, listaProductosPrestaShop, userOptionList);
+            }
+
+            case 2 -> {
+                pedirId(listaProductosPrestaShop);
+            }
+
+            case 3 -> {
+                pedirIdCombinacion(listaProductosPrestaShop);
+            }
+
+            case 4 -> {
+                pedirEan(listaProductosERP, listaProductosPrestaShop, userOptionList);
+            }
+
+            case 5 -> {
+                pedirCodReferencia(listaProductosERP, listaProductosPrestaShop, userOptionList);
+            }
+
+            case -1 -> {
+
+            }
+
+            default -> {
+                System.err.println("Introduzca un número entre el 1 y el 5");
+            }
         }
     }
 
@@ -223,5 +171,191 @@ public class BuscadorProductosService {
         System.out.println("Codigo de referencia: " + listaProductosPrestaShop.get(i).getCodReferencia() + "\n");
         System.out.println("Medida / Talla / Calibre: " + listaProductosPrestaShop.get(i).getTalla());
         System.out.println("Atributos: " + listaProductosPrestaShop.get(i).getAtributos() + "\n");
+    }
+
+    public static void pedirNombre(ArrayList<ProductoERP> listaProductosERP, ArrayList<ProductoPrestaShop> listaProductosPrestaShop, int userOptionList) {
+        String nombre = "";
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el nombre a buscar en la lista: ");
+        nombre = Main.scanner();
+
+        if (userOptionList == 1) {
+            for (int i = 0; i < listaProductosERP.size(); i++) {
+                if (listaProductosERP.get(i).getNombre().contains(nombre)) {
+                    mostrarInfoProductosERPEncontrado(listaProductosERP, i);
+                    encontrado = true;
+                }
+            }
+            if (!encontrado) {
+                System.err.println("No se encontró el producto que esta buscando!\n");
+            }
+        } else if (userOptionList == 2) {
+            for (int i = 0; i < listaProductosPrestaShop.size(); i++) {
+                if (listaProductosPrestaShop.get(i).getNombre().contains(nombre)) {
+                    mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
+                    encontrado = true;
+                }
+            }
+            if (!encontrado) {
+                System.err.println("No se encontró el producto que esta buscando!\n");
+            }
+        }
+    }
+
+    public static void pedirCodigo(ArrayList<ProductoERP> listaProductosERP) {
+        String codigo = "";
+        int i = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el codigo a buscar en la lista: ");
+        codigo = Main.scanner();
+
+        while (i < listaProductosERP.size() && !encontrado) {
+            if (codigo.equals(listaProductosERP.get(i).getCodigo())) {
+                mostrarInfoProductosERPEncontrado(listaProductosERP, i);
+                encontrado = true;
+            }
+            i++;
+        }
+        if (!encontrado) {
+            System.err.println("No se encontró el producto que esta buscando!\n");
+        }
+    }
+
+    public static void pedirEan(ArrayList<ProductoERP> listaProductosERP, ArrayList<ProductoPrestaShop> listaProductosPrestaShop, int userOptionList) {
+        String ean = "";
+        int i = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el Codigo de Barras / Ean a buscar en la lista: ");
+        ean = Main.scanner();
+
+        if (userOptionList == 1) {
+            while (i < listaProductosERP.size() && !encontrado) {
+                if (ean.equals(listaProductosERP.get(i).getEan())) {
+                    mostrarInfoProductosERPEncontrado(listaProductosERP, i);
+                    encontrado = true;
+                }
+                i++;
+            }
+            if (!encontrado) {
+                System.err.println("No se encontró el producto que esta buscando!\n");
+            }
+        } else if (userOptionList == 2) {
+            while (i < listaProductosPrestaShop.size() && !encontrado) {
+                if (ean.equals(listaProductosPrestaShop.get(i).getEan())) {
+                    mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
+                    encontrado = true;
+                }
+                i++;
+            }
+            if (!encontrado) {
+                System.err.println("No se encontró el producto que esta buscando!\n");
+            }
+        }
+    }
+
+    public static void pedirCodReferencia(ArrayList<ProductoERP> listaProductosERP, ArrayList<ProductoPrestaShop> listaProductosPrestaShop, int userOptionList) {
+        String codReferencia = "";
+        int i = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el codReferencia a buscar en la lista: ");
+        codReferencia = Main.scanner();
+
+        if (userOptionList == 1) {
+            while (i < listaProductosERP.size() && !encontrado) {
+                if (codReferencia.equals(listaProductosERP.get(i).getCodReferencia())) {
+                    mostrarInfoProductosERPEncontrado(listaProductosERP, i);
+                    encontrado = true;
+                }
+                i++;
+            }
+            if (!encontrado) {
+                System.err.println("No se encontró el producto que esta buscando!\n");
+            }
+        } else if (userOptionList == 2) {
+            while (i < listaProductosPrestaShop.size() && !encontrado) {
+                if (codReferencia.equals(listaProductosPrestaShop.get(i).getCodReferencia())) {
+                    mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
+                    encontrado = true;
+                }
+                i++;
+            }
+            if (!encontrado) {
+                System.err.println("No se encontró el producto que esta buscando!\n");
+            }
+        }
+    }
+
+    public static void pedirNombreProveedor(ArrayList<ProductoERP> listaProductosERP) {
+        String nombreProveedor = "";
+        int i = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el nombreProveedor a buscar en la lista: ");
+        nombreProveedor = Main.scanner();
+
+        while (i < listaProductosERP.size() && !encontrado) {
+            if (nombreProveedor.equals(listaProductosERP.get(i).getNombreProveedor())) {
+                mostrarInfoProductosERPEncontrado(listaProductosERP, i);
+                encontrado = true;
+            }
+            i++;
+        }
+        if (!encontrado) {
+            System.err.println("No se encontró el producto que esta buscando!\n");
+        }
+    }
+
+    public static void pedirId(ArrayList<ProductoPrestaShop> listaProductosPrestaShop) {
+        int id = 0,  i = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el ID a buscar en la lista: ");
+
+        try {
+            id = Integer.parseInt(Main.scanner());
+        } catch (NumberFormatException e) {
+            id = -1;
+            System.err.println("Solo se permiten numeros en este campo!");
+        }
+
+        while (i < listaProductosPrestaShop.size() && !encontrado) {
+            if (id == listaProductosPrestaShop.get(i).getId()) {
+                mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
+                encontrado = true;
+            }
+            i++;
+        }
+        if (!encontrado) {
+            System.err.println("No se encontró el producto que esta buscando!\n");
+        }
+    }
+
+    public static void pedirIdCombinacion(ArrayList<ProductoPrestaShop> listaProductosPrestaShop) {
+        int idCombinacion = 0,  i = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduzca el ID Combinacion a buscar en la lista: ");
+
+        try {
+            idCombinacion = Integer.parseInt(Main.scanner());
+        } catch (NumberFormatException e) {
+            idCombinacion = -1;
+            System.err.println("Solo se permiten numeros en este campo!");
+        }
+
+        while (i < listaProductosPrestaShop.size() && !encontrado) {
+            if (idCombinacion == listaProductosPrestaShop.get(i).getIdCombinacion()) {
+                mostrarInfoProductosPrestaShopEncontrado(listaProductosPrestaShop, i);
+                encontrado = true;
+            }
+            i++;
+        }
+        if (!encontrado) {
+            System.err.println("No se encontró el producto que esta buscando!\n");
+        }
     }
 }
